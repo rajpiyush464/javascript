268e2b3b668a16645214d4d2570d29e1f78fc1b6
Piyush Raj <132554272+rajpiyush464@users.noreply.github.com>
2024-06-08 11:18:41 +0000

    typeof

---
+++ basics/datatypes.js
@@ -0,0 +1,8 @@
+"use strict";   // treats every javacode as new
+
+let name="piyush"
+let num=2430
+let  bool=true
+let state
+console.log(typeof(null)) // object
+console.log(typeof(undefined)) // undefined
