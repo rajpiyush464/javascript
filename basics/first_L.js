0d3461bc6f518b41e57024a8f987a9e81a865f56
Piyush Raj <132554272+rajpiyush464@users.noreply.github.com>
2024-06-08 11:08:12 +0000

    container config

---
+++ basics/first_L.js
@@ -0,0 +1,10 @@
+const nume=12344
+let email="piyushraj5190@gmail.com"
+var name= "piyush"
+console.log(nume)
+console.log(email)
+console.log(name)
+console.table([nume,email,name])
+
+// Always use let in place of var because of scope concept //
+// constant an not be changed //
